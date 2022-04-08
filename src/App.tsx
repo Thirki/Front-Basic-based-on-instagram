0638c67c1b5070a82dbf6ef711db604a8fbc56bd
Thirki <henriquegomesdev@outlook.com>
2022-04-08 17:44:14 -0300

    Feat: add routes to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-import { Header } from "./components/Header";
-import { PostContainer } from "./components/PostContainer";
+import AppRoutes from "./AppRoutes";
 import { PostContextProvider } from "./context/PostsContext";
 
 function App() {
   return (
     <PostContextProvider>
       <div className="App">
-        <Header />
-        <PostContainer />
+        <AppRoutes />
       </div>
     </PostContextProvider>
   );
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Header } from "../../components/Header";
+import { PostContainer } from "../../components/PostContainer";
+
+export function Home() {
+  return (
+    <>
+      <Header />
+      <PostContainer />
+    </>
+  );
+}
