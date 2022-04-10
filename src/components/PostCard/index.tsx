3efe32c9b86829ce4deacac4e4afa90490fbbc31
Thirki <henriquegomesdev@outlook.com>
2022-04-10 13:39:28 -0300

    Feat: change to grid and change hover logic

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaHeart, FaComment, FaCalendarAlt } from "react-icons/fa";
 
 import { convertToDateFormater } from "../../helpers/convertToDateFormater";
@@ -11,27 +11,36 @@ interface IPostCardProps {
 }
 
 export function PostCard({ postObject }: IPostCardProps) {
-  console.log(postObject);
+  const [hoverImg, setHoverImg] = useState(false);
   return (
-    <a href={postObject.link} target='_blank="true"'>
-      <div className="image-card">
-        <div className="image-container">
-          <img src={postObject.imagens.resolucaoPadrao.url} alt="" />
-          <div className="post-status">
-            <p>@{postObject.usuario.username}</p>
-            <p>
-              <FaHeart />
-              {postObject.upvotes}
-            </p>
-            <p>
-              <FaComment />
-              {postObject.comentarios}
-            </p>
-            <p>
-              <FaCalendarAlt />
-              {convertToDateFormater(postObject.criadoEm)}
-            </p>
-          </div>
+    <a
+      href={postObject.link}
+      target='_blank="true"'
+      onMouseEnter={() => setHoverImg(true)}
+      onMouseLeave={() => setHoverImg(false)}
+    >
+      <div
+        className="image-container"
+        style={{
+          backgroundImage: hoverImg
+            ? `linear-gradient(to bottom,rgba(0,0, 0, .5), rgba(0,0, 0, .5)),url(${postObject.imagens.resolucaoPadrao.url})`
+            : `linear-gradient(to bottom,rgba(0,0, 0, 0), rgba(0,0, 0, .0)),url(${postObject.imagens.resolucaoPadrao.url})`,
+        }}
+      >
+        <div className="post-status">
+          <p>@{postObject.usuario.username}</p>
+          <p>
+            <FaHeart />
+            {postObject.upvotes}
+          </p>
+          <p>
+            <FaComment />
+            {postObject.comentarios}
+          </p>
+          <p>
+            <FaCalendarAlt />
+            {convertToDateFormater(postObject.criadoEm)}
+          </p>
         </div>
       </div>
     </a>
